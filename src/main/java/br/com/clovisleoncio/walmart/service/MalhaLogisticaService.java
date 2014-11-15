aee752d9bdab023979c854ea94e9d4eca9c23c80
clovisleoncio <clovisleoncio@gmail.com>
2014-11-14 22:02:19 -0200

    Recebimento de parâmetros na consulta

--- src/main/java/br/com/clovisleoncio/walmart/service/MalhaLogisticaService.java
+++ src/main/java/br/com/clovisleoncio/walmart/service/MalhaLogisticaService.java
@@ -1,13 +1,24 @@
 package br.com.clovisleoncio.walmart.service;
 
+import java.math.BigDecimal;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 @Path("/malhalogistica")
 public class MalhaLogisticaService {
 	
 	@GET
-	public String adicionar() {
+	public String consultar(@QueryParam("nome") String nome, @QueryParam("origem") String origem, @QueryParam("destino") String destino,
+			@QueryParam("autonomia") Integer autonomia, @QueryParam("valorLitro") BigDecimal valorLitro) {
+		
+		System.out.println(String.format("nome da malha: %s", nome));
+		System.out.println(String.format("Origem: %s", origem));
+		System.out.println(String.format("Destino: %s", destino));
+		System.out.println(String.format("Autonomia: %s", autonomia));
+		System.out.println(String.format("Valor do Litro: %s", valorLitro));
+
 		return "Chegou";
 	}
 
