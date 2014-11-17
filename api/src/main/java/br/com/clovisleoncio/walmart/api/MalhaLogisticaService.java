e3cae82169afd6be6282cb2f00093daf2fb79fb2
clovisleoncio <clovisleoncio@gmail.com>
2014-11-17 14:39:21 -0200

    Recebendo parâmetros da maneira correta

--- api/src/main/java/br/com/clovisleoncio/walmart/api/MalhaLogisticaService.java
+++ api/src/main/java/br/com/clovisleoncio/walmart/api/MalhaLogisticaService.java
@@ -3,8 +3,8 @@ package br.com.clovisleoncio.walmart.api;
 import java.math.BigDecimal;
 
 import javax.ejb.EJB;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.MatrixParam;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
@@ -33,19 +33,12 @@ public class MalhaLogisticaService {
 	}
 	
 	@PUT
-	public String adicionar(@MatrixParam("nome") String nome, @MatrixParam("malha") String malha) {
+	public String adicionar(@FormParam("nome") String nome, @FormParam("malha") String malha) {
 		System.out.println(String.format("Nome da malha: %s", nome));
 		System.out.println(String.format("Malha: %s", malha));
 
-		String exemplo = "A B 10\n"
-				+ "B D 15\n"
-				+ "A C 20\n"
-				+ "C D 30\n"
-				+ "B E 50\n"
-				+ "D E 30";
-		
 		MalhaLogisticaBuilder builder = new MalhaLogisticaBuilder();
-		MalhaLogistica malhaLogistica = builder.build("Exemplo", exemplo);
+		MalhaLogistica malhaLogistica = builder.build(nome, malha);
 		
 		malhaLogisticaEJB.gravar(malhaLogistica);
 
